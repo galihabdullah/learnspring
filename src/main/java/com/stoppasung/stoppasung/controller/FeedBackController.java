2d2b540b6fac87815ed5761a3aea4e41c990dd1a
galih abdullah <galihabdullah471@gmail.com>
2019-01-31 15:04:15 +0700

    coba ci/cd

--- src/main/java/com/stoppasung/stoppasung/controller/FeedBackController.java
+++ src/main/java/com/stoppasung/stoppasung/controller/FeedBackController.java
@@ -1,35 +1,35 @@
-//package com.stoppasung.stoppasung.controller;
-//
-//import com.stoppasung.stoppasung.Repository.LaporanRepository;
-//import com.stoppasung.stoppasung.model.FeedBackModel;
-//import com.stoppasung.stoppasung.model.pilihan.Role;
-//import com.stoppasung.stoppasung.services.FeedBackService;
-//import com.stoppasung.stoppasung.services.LaporanService;
-//import com.stoppasung.stoppasung.shared.dto.FeedBackDto;
-//import com.stoppasung.stoppasung.ui.model.request.FeedBackRequest;
-//import com.stoppasung.stoppasung.ui.model.response.FeedBackResponse;
-//import org.springframework.beans.BeanUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.validation.Valid;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.Optional;
-//
-//@RestController
-//@CrossOrigin
-//public class FeedBackController {
-//
-//
-//    @Autowired
-//    LaporanService laporanService;
-//
-//    @Autowired
-//    FeedBackService feedBackService;
-//
+package com.stoppasung.stoppasung.controller;
+
+import com.stoppasung.stoppasung.Repository.LaporanRepository;
+import com.stoppasung.stoppasung.model.FeedBackModel;
+import com.stoppasung.stoppasung.model.pilihan.Role;
+import com.stoppasung.stoppasung.services.FeedBackService;
+import com.stoppasung.stoppasung.services.LaporanService;
+import com.stoppasung.stoppasung.shared.dto.FeedBackDto;
+import com.stoppasung.stoppasung.ui.model.request.FeedBackRequest;
+import com.stoppasung.stoppasung.ui.model.response.FeedBackResponse;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+public class FeedBackController {
+
+
+    @Autowired
+    LaporanService laporanService;
+
+    @Autowired
+    FeedBackService feedBackService;
+
 //    @PostMapping("/laporandokter/{idLaporan}/dokter/feedback")
 //    public Map<String, Object> addFeedBackDokter(@PathVariable(value = "idLaporan") Long idLaporan,
 //                                  @Valid @RequestBody FeedBackRequest feedBackRequest
@@ -41,22 +41,22 @@
 //        returnValue.put("status", "sukses");
 //        return returnValue;
 //    }
-//
-//    @PostMapping("/laporan/{idLaporan}/pelapor/feeback")
-//    public Map<String, Object> addFeedBackPelapor(@PathVariable(value = "idLaporan") Long idLaporan,
-//                                                  @Valid @RequestBody FeedBackRequest feedBackRequest
-//                                                  ){
-//        FeedBackDto feedBackDto = new FeedBackDto();
-//        BeanUtils.copyProperties(feedBackRequest, feedBackDto);
-//        feedBackService.createFeedback(idLaporan, feedBackDto, Role.pelapor);
-//        Map<String, Object> returnValue = new HashMap<>();
-//        returnValue.put("Status", "sukses");
-//        return returnValue;
-//    }
-//
-//    @GetMapping("laporan/{idLaporan}/feedback")
-//    public List<FeedBackDto> getFeedBackByIdLaporan(@PathVariable(value = "idLaporan") Long idLaporan){
-//        return feedBackService.loadFeedBackByIdLaporan(idLaporan);
-//    }
-//
-//}
+
+    @PostMapping("/laporan/{idLaporan}/pelapor/feeback")
+    public Map<String, Object> addFeedBackPelapor(@PathVariable(value = "idLaporan") Long idLaporan,
+                                                  @Valid @RequestBody FeedBackRequest feedBackRequest
+                                                  ){
+        FeedBackDto feedBackDto = new FeedBackDto();
+        BeanUtils.copyProperties(feedBackRequest, feedBackDto);
+        feedBackService.createFeedback(idLaporan, feedBackDto, Role.pelapor);
+        Map<String, Object> returnValue = new HashMap<>();
+        returnValue.put("Status", "sukses");
+        return returnValue;
+    }
+
+    @GetMapping("laporan/{idLaporan}/feedback")
+    public List<FeedBackDto> getFeedBackByIdLaporan(@PathVariable(value = "idLaporan") Long idLaporan){
+        return feedBackService.loadFeedBackByIdLaporan(idLaporan);
+    }
+
+}
